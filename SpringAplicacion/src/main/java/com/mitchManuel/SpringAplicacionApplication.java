76a3299ce56533cb2c483ae15db7f6e17b21d6ca
86454 <86454@MITCH>
2021-06-28 23:24:45 -0500

    Commit inicial con Eclipse

---
+++ SpringAplicacion/src/main/java/com/mitchManuel/SpringAplicacionApplication.java
@@ -0,0 +1,13 @@
+package com.mitchManuel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAplicacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAplicacionApplication.class, args);
+	}
+
+}
