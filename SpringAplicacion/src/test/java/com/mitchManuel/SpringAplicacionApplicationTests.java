25e875d9a965952cba3b7540e0867c6014418189
86454 <86454@MITCH>
2021-06-29 19:01:06 -0500

    Commit inicial con Eclipse. Close !1

---
+++ SpringAplicacion/src/test/java/com/mitchManuel/SpringAplicacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mitchManuel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAplicacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
